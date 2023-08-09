eec975ee9455400f89b5c88e387676d95fa3e698
rafaelChelo <rafael@chelofinance.io>
2023-08-09 17:03:31 -0400

    added worldcoin bridge repo. Updated contracts

---
+++ contracts/scripts/deployMockEmitter.ts
@@ -0,0 +1,22 @@
+import hre, {ethers} from 'hardhat'
+import {writeJsonFile} from './utils'
+
+async function main() {
+    const MockEmitterFactory = await ethers.getContractFactory('MockEventEmitter')
+    const emitter = await MockEmitterFactory.deploy()
+
+    await emitter.deployed()
+
+    console.log(`MockEmitter: ${emitter.address}`)
+    writeJsonFile({
+        path: `/addresses.${hre.network.name}.json`,
+        data: {
+            Emitter: emitter.address,
+        },
+    })
+}
+
+main().catch(error => {
+    console.error(error)
+    process.exitCode = 1
+})
